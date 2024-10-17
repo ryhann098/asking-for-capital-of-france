74a66361bd00a2a1b6eeb289de5ce1a547dfecb8
ryhann098 <40297224@hruc.ac.uk>
2024-10-17 11:55:13 +0100

    Add files via upload

---
+++ paris.py
@@ -0,0 +1,7 @@
+print("welcome to the quiz")
+answer=input("enter captal city of france")
+while answer != "paris":
+    answer=input("enter captal city of france")
+    
+if answer == "paris":
+    print("correct")
